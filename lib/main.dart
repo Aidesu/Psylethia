bd6b50e08f686f531dcd563e69ec7bfe0261a0fb
bdsalocin <bdsalocin@proton.me>
2025-11-24 10:07:02 +0100

    correction des erreurs de merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:psylethia/view/cart_page.dart';
+import 'package:psylethia/view/category_page.dart';
+import 'package:psylethia/view/category_view.dart';
+import 'package:psylethia/view/profile_page.dart';
 import 'package:psylethia/viewmodel/cart_page_view_model.dart';
 import './viewmodel/mushroom_view_model.dart';
 import 'view/main_page.dart';
@@ -35,13 +39,10 @@ class MyApp extends StatelessWidget {
         routes: {
           '/main': (_) => const MainPage(),
           '/home': (_) => const HomePage(),
-          // =======
-          //           '/home': (_) => const HomePage(),
-          //           '/category': (_) => const CategoryPage(),
-          //           '/profile': (_) => const ProfilePage(),
-          //           '/category/view': (_) => CategoryView(),
-          //           '/cart': (_) => const CartPage(),
-          // >>>>>>> 1ed165ecfc556859aa319936a5d17bc4a353652e
+          '/category': (_) => const CategoryPage(),
+          '/category/view': (_) => const CategoryView(),
+          '/profile': (_) => const ProfilePage(),
+          '/cart': (_) => const CartPage(),
         },
       ),
     );
--- lib/viewmodel/category_view_model.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:flutter/material.dart';
-// import '../model/category_model.dart';
-
-// class CategoryViewModel extends ChangeNotifier {
-//   List<Category> category = [];
-// }
